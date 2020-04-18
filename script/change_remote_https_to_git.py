609d5bdd727df2b85b6d701f6a857a319c249059
Mathieu Benoit <mathben@technolibre.ca>
2020-04-17 21:11:47 -0400

    WIP

--- script/change_remote_https_to_git.py
+++ script/change_remote_https_to_git.py
@@ -4,7 +4,13 @@
 
 import argparse
 import logging
+import os
+import sys
+
 from git import Repo
+
+new_path = os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(new_path)
 
 from script.git_tool import GitTool
 
@@ -17,7 +23,6 @@ def get_config():
 
     :return: dict of config file settings and command line arguments
     """
-    # config = get_project_config()
 
     # TODO update description
     parser = argparse.ArgumentParser(
@@ -31,6 +36,8 @@ def get_config():
                         help="Path of repo to change remote, including submodule.")
     parser.add_argument('-f', '--upstream', dest="upstream", default="origin",
                         help="Upstream name to change address https to git.")
+    parser.add_argument("--git_to_https", action="store_true",
+                        help="Replace all repo git to https.")
     args = parser.parse_args()
     return args
 
@@ -49,9 +56,11 @@ def main():
         print(f"Nb element {i}/{total}")
         repo_sm = Repo(repo.get("name"))
         remote_upstream_name = [a for a in repo_sm.remotes if upstream_name == a.name]
+        new_url = repo.get("url_https") if config.git_to_https else repo.get("url_git")
+
         for remote in remote_upstream_name:
-            remote.set_url(repo.get("url_git"))
-        print('Remote "%s" created for %s' % (upstream_name, repo.get("url_git")))
+            remote.set_url(new_url)
+        print('Remote "%s" created for %s' % (upstream_name, new_url))
 
 
 if __name__ == '__main__':
